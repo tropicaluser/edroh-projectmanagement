f503f6a4d63be796177a8e2c9ef75ec06978dd3b
tropicaluser <hallbergstephaniie@gmail.com>
2024-11-08 21:43:57 +0100

    feat(client): add sidebar - skeleton

--- client/src/app/DashboardLayout.tsx
+++ client/src/app/DashboardLayout.tsx
@@ -1,11 +1,10 @@
-"use client";
-
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      {/* sidebar */}
+      <Sidebar />
       <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg`}>
         <Navbar />
         {children}
