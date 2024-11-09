8af5c65d4c252e5e39b0918908b93deb9fc302dd
tropicaluser <hallbergstephaniie@gmail.com>
2024-11-09 15:34:49 +0100

    feat(client): project list view

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 import ProjectHeader from "@/app/projects/ProjectHeader";
 import Board from "../BoardView";
+import List from "../ListView";
 
 type Props = {
   params: { id: string };
@@ -20,6 +21,9 @@ const Project = ({ params }: Props) => {
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === "Board" && (
         <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+       {activeTab === "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )}
     </div>
   );
